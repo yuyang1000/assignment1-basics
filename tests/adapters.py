7b7867f558a6b49b0d759e508e5bf798c314eced
yy <10085163+meetyuyang@user.noreply.gitee.com>
2025-08-18 15:03:07 +0800

    just_use_torch_softmax

--- tests/adapters.py
+++ tests/adapters.py
@@ -766,7 +766,8 @@ def run_softmax(in_features: Float[Tensor, " ..."], dim: int) -> Float[Tensor, "
         Float[Tensor, "..."]: Tensor of with the same shape as `in_features` with the output of
         softmax normalizing the specified `dim`.
     """
-    raise NotImplementedError
+    import torch
+    return torch.softmax(in_features, dim=dim)
 
 
 def run_cross_entropy(
